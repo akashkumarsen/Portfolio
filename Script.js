375a8891b600de008d0e6cf7ce6e92a420891bc7
akashsinghsen <akashsingh78475@gmail.com>
2024-08-28 01:20:22 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,9 @@
+// Example JavaScript to smooth scroll on navigation link click
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
